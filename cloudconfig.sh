cb39db0455aa6b510ebcb93f7e56c76450496f1c
spavan939 <spavan939@gmail.com>
2018-01-19 16:09:49 +0530

    Create cloudconfig.sh

---
+++ cloudconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo su
+
+Test=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/resourceGroupName?api-version=2017-08-01&format=text")
+
+sleep 2
+
+sed -i 's/AVI-RESOURCE-GROUP/'"$Test"'/g' /tmp/user_data.json
+
+sleep 1
+
+reboot
