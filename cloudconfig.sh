11e35ba8e3ee4dd0a4f522f830f9429d82ae8f91
spavan939 <spavan939@gmail.com>
2018-01-19 17:27:39 +0530

    Update cloudconfig.sh

--- cloudconfig.sh
+++ cloudconfig.sh
@@ -2,6 +2,8 @@
 
 sudo su
 
+cp /var/lib/cloud/instance/user-data.txt /tmp/user_data.json
+
 Test=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/resourceGroupName?api-version=2017-08-01&format=text")
 
 sleep 2
