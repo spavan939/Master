a121e91c56d2e2c1e3ecacf270ea3da648a1bf94
spavan939 <spavan939@gmail.com>
2018-01-22 12:22:39 +0530

    Update cloudconfig.sh

--- cloudconfig.sh
+++ cloudconfig.sh
@@ -2,13 +2,11 @@
 
 sudo su
 
-rm /var/lib/avi/etc/.setup.json.done
-
 Test=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/resourceGroupName?api-version=2017-08-01&format=text")
 
 sleep 2
 
-sed -i 's/AVI-RESOURCE-GROUP/'"$Test"'/g' /var/lib/cloud/instance/user-data.txt
+sed -i 's/AVI-RESOURCE-GROUP/'"$Test"'/g' /tmp/user_data.json
 
 sleep 2
 
